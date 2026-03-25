fc5fb6ad6fb2a138531d9dca9a5030f92cdecf4b
gabriel <dbsuperss@gmail.com>
2026-03-25 17:40:25 +0900

    new

---
+++ projects/digital-lego/electron/main.cjs
@@ -0,0 +1,36 @@
+'use strict'
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173')
+    win.webContents.openDevTools()
+  } else {
+    win.loadFile(path.join(app.getAppPath(), 'dist/index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
